4efd7ab97f31f5dab5997df116e332851ca69612
haesleinhuepf <rhaase@mpi-cbg.de>
2017-10-28 22:23:32 +0200

    made parameters configurable from UI

--- src/clearcontrol/microscope/lightsheet/extendeddepthfield/DepthOfFocusImagingEngine.java
+++ src/clearcontrol/microscope/lightsheet/extendeddepthfield/DepthOfFocusImagingEngine.java
@@ -4,6 +4,7 @@ import clearcontrol.core.device.task.TaskDevice;
 import clearcontrol.core.log.LoggingFeature;
 import clearcontrol.core.variable.Variable;
 import clearcontrol.core.variable.bounded.BoundedVariable;
+import clearcontrol.devices.cameras.StackCameraDeviceInterface;
 import clearcontrol.gui.jfx.custom.visualconsole.VisualConsoleInterface;
 import clearcontrol.microscope.lightsheet.LightSheetMicroscope;
 import clearcontrol.microscope.lightsheet.LightSheetMicroscopeQueue;
@@ -16,49 +17,117 @@ import gnu.trove.list.array.TDoubleArrayList;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-
 /**
  * @author haesleinhuepf
- *
- *
  */
 public class DepthOfFocusImagingEngine extends TaskDevice implements
                                                           LoggingFeature,
                                                           VisualConsoleInterface
 {
+  private BoundedVariable<Integer> mDetectionArmIndex;
+  private BoundedVariable<Integer> mLightSheetIndex;
+  private BoundedVariable<Integer>
+      mNumberOfISamples =
+      new BoundedVariable<Integer>("Number of illumination samples",
+                                   10,
+                                   0,
+                                   Integer.MAX_VALUE,
+                                   1);
+  private BoundedVariable<Integer>
+      mNumberOfDSamples =
+      new BoundedVariable<Integer>("Number of detection samples",
+                                   10,
+                                   0,
+                                   Integer.MAX_VALUE,
+                                   1);
 
+  private final BoundedVariable<Double>
+      mExposureVariableInSeconds =
+      new BoundedVariable<Double>("Exposure time (s)",
+                                  1.0,
+                                  0.0,
+                                  Double.POSITIVE_INFINITY,
+                                  0.0);
 
+  private Variable<String>
+      mDataSetNamePostfixVariable =
+      new Variable<String>("Test");
+  private Variable<File>
+      mRootFolderVariable =
+      new Variable("RootFolder", (Object) null);
 
+  public BoundedVariable<Integer> getDetectionArmIndex()
+  {
+    return mDetectionArmIndex;
+  }
+
+  public BoundedVariable<Integer> getLightSheetIndex()
+  {
+    return mLightSheetIndex;
+  }
+
+  public BoundedVariable<Integer> getNumberOfISamples()
+  {
+    return mNumberOfISamples;
+  }
+
+  public BoundedVariable<Integer> getNumberOfDSamples()
+  {
+    return mNumberOfDSamples;
+  }
+
+  public BoundedVariable<Double> getExposureVariable()
+  {
+    return mExposureVariableInSeconds;
+  }
+
+  public Variable<String> getDataSetNamePostfixVariable()
+  {
+    return mDataSetNamePostfixVariable;
+  }
+
+  public Variable<File> getRootFolderVariable()
+  {
+    return mRootFolderVariable;
+  }
 
   private final LightSheetMicroscope mLightSheetMicroscope;
-
-
-  private final Variable<String> mCalibrationDataName =
-      new Variable<String>("TargetFolder",
-                           "C:\temp");
-
 
   public DepthOfFocusImagingEngine(LightSheetMicroscope pLightSheetMicroscope)
   {
     super("DepthOfFocusImagingEngine");
     mLightSheetMicroscope = pLightSheetMicroscope;
-    this.setName("DepthOfFocusImagingEngine");
+    this.setName("ExtdDepthOfFocusImaging");
+
+    mDetectionArmIndex =
+        new BoundedVariable<Integer>("Detection arm",
+                                     0,
+                                     0,
+                                     mLightSheetMicroscope.getNumberOfDetectionArms(),
+                                     1);
+
+    mLightSheetIndex =
+        new BoundedVariable<Integer>("Light sheet",
+                                     0,
+                                     0,
+                                     mLightSheetMicroscope.getNumberOfLightSheets(),
+                                     1);
+
   }
 
-  @Override
-  public boolean startTask()
+  @Override public boolean startTask()
   {
     if (getLightSheetMicroscope().getCurrentTask().get() != null)
     {
-      warning("Another task (%s) is already running, please stop it first.",
-              getLightSheetMicroscope().getCurrentTask());
+      warning(
+          "Another task (%s) is already running, please stop it first.",
+          getLightSheetMicroscope().getCurrentTask());
       return false;
     }
     getLightSheetMicroscope().getCurrentTask().set(this);
@@ -97,24 +166,34 @@ public class DepthOfFocusImagingEngine extends TaskDevice implements
   private boolean image() throws
                           InterruptedException,
                           ExecutionException,
-                          TimeoutException, IOException
+                          TimeoutException,
+                          IOException
   {
     if (isStopRequested())
       return false;/**/
 
-    int lDetectionArmIndex = 0;
-    int lLightSheetIndex = 0;
+    int lDetectionArmIndex = mDetectionArmIndex.get();
+    int lLightSheetIndex = mLightSheetIndex.get();
 
-    int lNumberOfISamples = 10;
-    int lNumberOfDSamples = 10;
+    int lNumberOfISamples = mNumberOfISamples.get();
+    int lNumberOfDSamples = mNumberOfDSamples.get();
 
-    int lImageWidth = 512;
-    int lImageHeight = 512;
+    long
+        lImageWidth = (Long)
+        getLightSheetMicroscope().getDevice(StackCameraDeviceInterface.class,
+                                            0).getStackWidthVariable().get();
+    long
+        lImageHeight =(Long)
+        getLightSheetMicroscope().getDevice(StackCameraDeviceInterface.class,
+                                            0).getStackHeightVariable().get();
 
-    double lExposureTimeInSeconds = 1;
 
-    String lFoldername = "C:/structure/temp/images/";
-    String lDatasetname = "Test" + new Date();
+    double lExposureTimeInSeconds = mExposureVariableInSeconds.get();
+
+    //String lFoldername = "C:/structure/temp/images/";
+    String
+        lDatasetname =
+        getDataSetNamePostfixVariable().get() + new SimpleDateFormat("yyyy.MM.dd HH-mm-ss").format(new Date());
 
     BoundedVariable<Number>
         lDetectionFocusZVariable =
@@ -123,14 +202,14 @@ public class DepthOfFocusImagingEngine extends TaskDevice implements
                                             lDetectionArmIndex)
                                  .getZVariable();
 
-
-    LightSheetInterface lLightSheetDevice =
+    LightSheetInterface
+        lLightSheetDevice =
         getLightSheetMicroscope().getDeviceLists()
                                  .getDevice(LightSheetInterface.class,
                                             lLightSheetIndex);
 
-
-    BoundedVariable<Number> lZVariable =
+    BoundedVariable<Number>
+        lZVariable =
         lLightSheetDevice.getZVariable();
     double lMinIZ = lZVariable.getMin().doubleValue();
     double lMaxIZ = lZVariable.getMax().doubleValue();
@@ -142,8 +221,8 @@ public class DepthOfFocusImagingEngine extends TaskDevice implements
 
     double lStep = (lMaxDZ - lMinDZ) / (lNumberOfDSamples - 1);
 
-
-    LightSheetMicroscopeQueue lQueue =
+    LightSheetMicroscopeQueue
+        lQueue =
         getLightSheetMicroscope().requestQueue();
 
     // Initialize ----------------------------------------------------
@@ -151,7 +230,7 @@ public class DepthOfFocusImagingEngine extends TaskDevice implements
     // lQueue.zero();
 
     lQueue.setFullROI();
-    lQueue.setCenteredROI(lImageWidth, lImageHeight);
+    lQueue.setCenteredROI((int)lImageWidth, (int)lImageHeight);
     lQueue.setExp(lExposureTimeInSeconds);
 
     // set to an initial state ---------------------------------------
@@ -163,8 +242,8 @@ public class DepthOfFocusImagingEngine extends TaskDevice implements
     //lQueue.setIH(mLightSheetIndex, 0);
 
     lQueue.setIZ(lLightSheetIndex, lMinDZ);
-    lQueue.setDZ(lDetectionArmIndex /*?*/, lMinDZ);
-    lQueue.setC(lDetectionArmIndex /*?*/, false);
+    lQueue.setDZ(lDetectionArmIndex, lMinDZ);
+    lQueue.setC(lDetectionArmIndex, false);
 
     lQueue.addCurrentStateToQueue();
 
@@ -200,40 +279,39 @@ public class DepthOfFocusImagingEngine extends TaskDevice implements
 
     // read out ------------------------------------------------------
     getLightSheetMicroscope().useRecycler("adaptation", 1, 4, 4);
-    final Boolean lPlayQueueAndWait =
+    final Boolean
+        lPlayQueueAndWait =
         getLightSheetMicroscope().playQueueAndWaitForStacks(lQueue,
-                                                            100 + lQueue.getQueueLength(),
+                                                            100
+                                                            + lQueue.getQueueLength(),
                                                             TimeUnit.SECONDS);
 
-    if (lPlayQueueAndWait) {
-      final OffHeapPlanarStack lStack =
+    if (lPlayQueueAndWait)
+    {
+      final OffHeapPlanarStack
+          lStack =
           (OffHeapPlanarStack) getLightSheetMicroscope().getCameraStackVariable(
-              lDetectionArmIndex)
-                                                        .get();
+              lDetectionArmIndex).get();
 
-      if (lStack != null) {
+      if (lStack != null)
+      {
         //ContiguousMemoryInterface memory = lStack.getContiguousMemory();
 
-       // long lWidth = lStack.getWidth();
-       // long lHeight = lStack.getHeight();
-       // long lDepth = lStack.getDepth();
+        // long lWidth = lStack.getWidth();
+        // long lHeight = lStack.getHeight();
+        // long lDepth = lStack.getDepth();
 
         RawFileStackSink sink = new RawFileStackSink();
-        sink.setLocation(new File(lFoldername), lDatasetname);
+        sink.setLocation(mRootFolderVariable.get(), lDatasetname);
+        System.out.println(mRootFolderVariable.get() + lDatasetname);
         sink.appendStack(lStack);
 
         sink.close();
-
 
       }
     }
     System.out.println("Bye.");
     return true;
-  }
-
-  public Variable<String> getCalibrationDataNameVariable()
-  {
-    return mCalibrationDataName;
   }
 
   /**
--- src/clearcontrol/microscope/lightsheet/extendeddepthfield/gui/DepthOfFieldFocusImagingEngineToolbar.java
+++ src/clearcontrol/microscope/lightsheet/extendeddepthfield/gui/DepthOfFieldFocusImagingEngineToolbar.java
@@ -1,17 +1,161 @@
 package clearcontrol.microscope.lightsheet.extendeddepthfield.gui;
 
+import clearcontrol.core.variable.bounded.BoundedVariable;
+import clearcontrol.devices.cameras.StackCameraDeviceInterface;
+import clearcontrol.devices.cameras.gui.CameraResolutionGrid;
 import clearcontrol.gui.jfx.custom.gridpane.CustomGridPane;
+import clearcontrol.gui.jfx.var.combo.ClassComboBoxVariable;
+import clearcontrol.gui.jfx.var.file.VariableFileChooser;
+import clearcontrol.gui.jfx.var.textfield.NumberVariableTextField;
+import clearcontrol.gui.jfx.var.textfield.StringVariableTextField;
 import clearcontrol.microscope.lightsheet.extendeddepthfield.DepthOfFocusImagingEngine;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
 public class DepthOfFieldFocusImagingEngineToolbar extends
                                                    CustomGridPane
 {
-  public DepthOfFieldFocusImagingEngineToolbar(DepthOfFocusImagingEngine pDepthOfFieldImagingEngine) {
+  public DepthOfFieldFocusImagingEngineToolbar(
+      DepthOfFocusImagingEngine pDepthOfFieldImagingEngine)
+  {
     int lRow = 0;
+
+    {
+      addIntegerField(pDepthOfFieldImagingEngine.getDetectionArmIndex(), lRow);
+      lRow++;
+    }
+
+    {
+      addIntegerField(pDepthOfFieldImagingEngine.getLightSheetIndex(), lRow);
+      lRow++;
+    }
+
+    {
+      addIntegerField(pDepthOfFieldImagingEngine.getNumberOfISamples(), lRow);
+      lRow++;
+    }
+
+    {
+      addIntegerField(pDepthOfFieldImagingEngine.getNumberOfDSamples(), lRow);
+      lRow++;
+    }
+
+    {
+      addDoubleField(pDepthOfFieldImagingEngine.getExposureVariable(), lRow);
+      lRow++;
+    }
+
+    {
+      VariableFileChooser
+          lRootFolderChooser =
+          new VariableFileChooser("Folder:",
+                                  pDepthOfFieldImagingEngine.getRootFolderVariable(),
+                                  true);
+      GridPane.setColumnSpan(lRootFolderChooser.getLabel(), Integer.valueOf(1));
+      GridPane.setColumnSpan(lRootFolderChooser.getTextField(),
+                             Integer.valueOf(2));
+      GridPane.setColumnSpan(lRootFolderChooser.getButton(), Integer.valueOf(1));
+      this.add(lRootFolderChooser.getLabel(), 0, lRow);
+      this.add(lRootFolderChooser.getTextField(), 1, lRow);
+      this.add(lRootFolderChooser.getButton(), 3, lRow);
+
+      lRow++;
+    }
+
+    {
+      StringVariableTextField
+          lPostFixTextField = new StringVariableTextField("Name:", pDepthOfFieldImagingEngine.getDataSetNamePostfixVariable());
+      /*ClassComboBoxVariable
+          lStackSinkComboBox = new ClassComboBoxVariable(pTimelapseInterface.getCurrentFileStackSinkTypeVariable(), pTimelapseInterface.getFileStackSinkTypeList(), 100);*/
+      GridPane.setColumnSpan(lPostFixTextField.getLabel(), Integer.valueOf(1));
+      GridPane.setColumnSpan(lPostFixTextField.getTextField(), Integer.valueOf(2));
+      /*GridPane.setColumnSpan(lStackSinkComboBox, Integer.valueOf(1));*/
+      this.add(lPostFixTextField.getLabel(), 0, lRow);
+      this.add(lPostFixTextField.getTextField(), 1, lRow);
+      /*this.add(lStackSinkComboBox, 3, lRow);*/
+
+      lRow++;
+    }
+
+
+    {
+
+      CameraResolutionGrid.ButtonEventHandler lButtonHandler =
+          (w,
+           h) -> {
+            return event -> {
+              pDepthOfFieldImagingEngine.getLightSheetMicroscope()
+                                     .setCameraWidthHeight(w,
+                                                           h);
+            };
+          };
+
+      final int lMaxCameraWidth =
+          pDepthOfFieldImagingEngine.getLightSheetMicroscope()
+                                 .getDevice(StackCameraDeviceInterface.class,
+                                            0)
+                                 .getMaxWidthVariable()
+                                 .get()
+                                 .intValue();
+      final int lMaxCameraHeight =
+          pDepthOfFieldImagingEngine.getLightSheetMicroscope()
+                                 .getDevice(StackCameraDeviceInterface.class,
+                                            0)
+                                 .getMaxHeightVariable()
+                                 .get()
+                                 .intValue();
+
+      CameraResolutionGrid lGridPane =
+          new CameraResolutionGrid(lButtonHandler,
+                                   lMaxCameraWidth,
+                                   lMaxCameraHeight);
+      lGridPane.setAlignment(Pos.BASELINE_CENTER);
+      GridPane.setHalignment(lGridPane, HPos.CENTER);
+      GridPane.setHgrow(lGridPane, Priority.ALWAYS);
+      GridPane.setColumnSpan(lGridPane, 3);
+      add(lGridPane, 0, lRow);
+
+      // setGridLinesVisible(true);
+
+      lRow++;
+    }
+//
+//    {
+//      TextField
+//          lCalibrationDataNameTextField =
+//          new TextField(pCalibrationEngine.getCalibrationDataNameVariable()
+//                                          .get());
+//      lCalibrationDataNameTextField.setMaxWidth(Double.MAX_VALUE);
+//      lCalibrationDataNameTextField.textProperty()
+//                                   .addListener((obs, o, n) -> {
+//                                     String lName = n.trim();
+//                                     if (!lName.isEmpty())
+//                                       pCalibrationEngine.getCalibrationDataNameVariable()
+//                                                         .set(lName);
+//
+//                                   });
+//      GridPane.setColumnSpan(lCalibrationDataNameTextField, 3);
+//      GridPane.setFillWidth(lCalibrationDataNameTextField, true);
+//      GridPane.setHgrow(lCalibrationDataNameTextField,
+//                        Priority.ALWAYS);
+//      add(lCalibrationDataNameTextField, 0, lRow);
+//
+//      lRow++;
+//    }
+
+    {
+      Separator lSeparator = new Separator();
+      lSeparator.setOrientation(Orientation.HORIZONTAL);
+      GridPane.setColumnSpan(lSeparator, 4);
+      add(lSeparator, 0, lRow);
+      lRow++;
+    }
 
     {
       Button lStart = new Button("Start");
@@ -26,5 +170,29 @@ public class DepthOfFieldFocusImagingEngineToolbar extends
 
       lRow++;
     }
+  }
+
+  private void addIntegerField(BoundedVariable<Integer> variable, int lRow) {
+    NumberVariableTextField<Integer>
+        lField =
+        new NumberVariableTextField<Integer>(variable.getName(),
+                                            variable,
+                                            variable.getMin(),
+                                            variable.getMax(),
+                                            variable.getGranularity());
+    this.add(lField.getLabel(), 0, lRow);
+    this.add(lField.getTextField(), 1, lRow);
+
+  }
+  private void addDoubleField(BoundedVariable<Double> variable, int lRow) {
+    NumberVariableTextField<Double>
+        lField =
+        new NumberVariableTextField<Double>(variable.getName(),
+            variable,
+            variable.getMin(),
+            variable.getMax(),
+            variable.getGranularity());
+    this.add(lField.getLabel(), 0, lRow);
+    this.add(lField.getTextField(), 1, lRow);
   }
 }
