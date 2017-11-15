987ee98302b4617a6e69347129d4f2bd6db14bbc
haesleinhuepf <rhaase@mpi-cbg.de>
2017-11-15 11:47:19 +0100

    delete the QueuePrinter, it was just for debugging

--- src/clearcontrol/microscope/lightsheet/timelapse/LightSheetTimelapse.java
+++ src/clearcontrol/microscope/lightsheet/timelapse/LightSheetTimelapse.java
@@ -7,7 +7,6 @@ import java.util.concurrent.TimeoutException;
 
 import clearcontrol.core.log.LoggingFeature;
 import clearcontrol.core.variable.Variable;
-import clearcontrol.microscope.QueuePrinter;
 import clearcontrol.microscope.lightsheet.LightSheetMicroscope;
 import clearcontrol.microscope.lightsheet.LightSheetMicroscopeQueue;
 import clearcontrol.microscope.lightsheet.processor.MetaDataFusion;
