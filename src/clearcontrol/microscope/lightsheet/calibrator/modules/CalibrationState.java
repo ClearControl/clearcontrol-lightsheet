54779e731ec28b17867b55d27aacd8323a36c0f8
Haesleinhuepf <rhaase@mpi-cbg.de>
2017-11-24 11:45:25 +0100

    introduce calibration state handling to inform the user about success

---
+++ src/clearcontrol/microscope/lightsheet/calibrator/modules/CalibrationState.java
@@ -0,0 +1,12 @@
+package clearcontrol.microscope.lightsheet.calibrator.modules;
+
+/**
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * November 2017
+ */
+public enum CalibrationState
+{
+  NOT_CALIBRATED,
+  SUCCEEDED,
+  FAILED
+}
