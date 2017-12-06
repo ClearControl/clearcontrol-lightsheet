10ed66992bd2feb467583d95ca9ba4fcb3611aeb
Haesleinhuepf <rhaase@mpi-cbg.de>
2017-12-06 15:06:14 +0100

    add a new scheme for distributing control planes

--- src/clearcontrol/microscope/lightsheet/state/ControlPlaneLayout.java
+++ src/clearcontrol/microscope/lightsheet/state/ControlPlaneLayout.java
@@ -15,6 +15,11 @@ public enum ControlPlaneLayout
   */
  Linear,
 
+  /**
+   * Linear layout, but the control plane in the center exists twice
+   */
+  LinearWithDoubledCenter,
+
  /**
   * Circular layout: control planes are laid with equal spacing on the unit
   * circle and then projected on the x axis.
@@ -40,6 +45,9 @@ public enum ControlPlaneLayout
     case Linear:
       return linear(pNumberOfControlPlanes, pControlPlaneIndex);
 
+    case LinearWithDoubledCenter:
+      return linearWithDoubledCenter(pNumberOfControlPlanes, pControlPlaneIndex);
+
     case Circular:
       return circular(pNumberOfControlPlanes, pControlPlaneIndex);
 
@@ -63,6 +71,28 @@ public enum ControlPlaneLayout
     double zc = 0.5 * (1 + cos(PI * (1 - z)));
 
     return zc;
+  }
+
+  private double linearWithDoubledCenter(int pNumberOfControlPlanes,
+                                         int pControlPlaneIndex) {
+    int lFirstHalfNumberOfControlPlanes = pNumberOfControlPlanes / 2;
+    int lSecondHalfNumberOfControlPlanes = pNumberOfControlPlanes - lFirstHalfNumberOfControlPlanes;
+
+    if (pControlPlaneIndex < lFirstHalfNumberOfControlPlanes) {
+
+      return ((double) pControlPlaneIndex)
+             / (lFirstHalfNumberOfControlPlanes - 1) / 2.0;
+    } else {
+      return 0.5 + ((double) (pControlPlaneIndex - lFirstHalfNumberOfControlPlanes))
+             / (lSecondHalfNumberOfControlPlanes - 1) / 2.0;
+    }
+  }
+
+  public static void main(String... args) {
+    for (int i = 0; i < 7; i++)
+    {
+      System.out.println("" + i +": " + LinearWithDoubledCenter.layout( 7, i));
+    }
   }
 
 }
