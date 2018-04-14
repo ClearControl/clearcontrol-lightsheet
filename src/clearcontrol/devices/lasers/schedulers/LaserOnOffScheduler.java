d45cd9035c37d7e5227ae8531eca541c74daf4c8
Haesleinhuepf <rhaase@mpi-cbg.de>
2018-04-14 09:12:55 +0200

    added LaserOnOffScheduler for multichannel imaging

---
+++ src/clearcontrol/devices/lasers/schedulers/LaserOnOffScheduler.java
@@ -0,0 +1,45 @@
+package clearcontrol.devices.lasers.schedulers;
+
+import clearcontrol.core.log.LoggingFeature;
+import clearcontrol.devices.lasers.LaserDeviceInterface;
+import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerBase;
+import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerInterface;
+
+/**
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * April 2018
+ */
+public class LaserOnOffScheduler extends SchedulerBase implements
+                                                       SchedulerInterface,
+                                                       LoggingFeature
+{
+  LaserDeviceInterface mLaserDevice;
+  boolean mTurnOn;
+
+  /**
+   * INstanciates a virtual device with a given name
+   *
+   */
+  public LaserOnOffScheduler(LaserDeviceInterface pLaserDevice, boolean pTurnOn)
+  {
+    super("Turn " + pLaserDevice.getName() + " " + (pTurnOn?"ON":"OFF"));
+    mLaserDevice = pLaserDevice;
+    mTurnOn = pTurnOn;
+  }
+
+  @Override public boolean initialize()
+  {
+    return true;
+  }
+
+  @Override public boolean enqueue(long pTimePoint)
+  {
+    mLaserDevice.setLaserPowerOn(true);
+    mLaserDevice.setLaserOn(true);
+    mLaserDevice.setLaserPowerOn(true);
+    mLaserDevice.setLaserOn(true);
+    return true;
+  }
+
+
+}
