03e6f6ee1f2f90df0b1aa09cf1b9b57e5d56dcb2
Haesleinhuepf <rhaase@mpi-cbg.de>
2018-04-14 09:20:39 +0200

    added Acquisition state file I/O + schedulers for I/O, backing up, restoring, resetting

---
+++ src/clearcontrol/microscope/lightsheet/state/schedulers/AcquisitionStateBackupRestoreScheduler.java
@@ -0,0 +1,66 @@
+package clearcontrol.microscope.lightsheet.state.schedulers;
+
+import clearcontrol.core.log.LoggingFeature;
+import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerBase;
+import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerInterface;
+import clearcontrol.microscope.state.AcquisitionStateInterface;
+
+import java.util.ArrayList;
+
+/**
+ * This scheduler allows saving a copy of the current acquisition state
+ * to a list or restoring the latest acquisition state if the list is
+ * not empty.
+ *
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * March 2018
+ */
+public class AcquisitionStateBackupRestoreScheduler extends
+                                                    SchedulerBase implements
+                                                                  SchedulerInterface,
+                                                                  LoggingFeature
+{
+  boolean mBackup;
+  private static ArrayList<AcquisitionStateInterface> mAcquisitionStateList = new ArrayList<>();
+  /**
+   * INstanciates a virtual device with a given name
+   *
+   * @Parameter pBackup: If true, the scheduler puts a new entry in
+   * the LIFO list, if false it will restore the last entry.
+   */
+  public AcquisitionStateBackupRestoreScheduler(boolean pBackup)
+  {
+    super("Acquisition state: " + (pBackup? ("Backup"):("Restore")));
+    mBackup = pBackup;
+  }
+
+  @Override public boolean initialize()
+  {
+    return true;
+  }
+
+  @Override public boolean enqueue(long pTimePoint)
+  {
+    if (mBackup) {
+      AcquisitionStateInterface lState = mMicroscope.getAcquisitionStateManager().getCurrentState().duplicate("backup " + System.currentTimeMillis());
+      mAcquisitionStateList.add(lState);
+    } else {
+      if (mAcquisitionStateList.size() > 0)
+      {
+        AcquisitionStateInterface
+            lState =
+            mAcquisitionStateList.get(mAcquisitionStateList.size() - 1);
+        mAcquisitionStateList.remove(mAcquisitionStateList.size() - 1);
+        mMicroscope.getAcquisitionStateManager().setCurrentState(lState);
+      } else {
+        warning("Error: Cannot restore state, list is empty.");
+      }
+    }
+    return true;
+  }
+
+  public boolean isBackup()
+  {
+    return mBackup;
+  }
+}
---
+++ src/clearcontrol/microscope/lightsheet/state/schedulers/AcquisitionStateResetScheduler.java
@@ -0,0 +1,56 @@
+package clearcontrol.microscope.lightsheet.state.schedulers;
+
+import clearcontrol.core.log.LoggingFeature;
+import clearcontrol.microscope.lightsheet.LightSheetDOF;
+import clearcontrol.microscope.lightsheet.LightSheetMicroscope;
+import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerBase;
+import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerInterface;
+import clearcontrol.microscope.lightsheet.state.InterpolatedAcquisitionState;
+import clearcontrol.microscope.lightsheet.state.tables.InterpolationTables;
+
+/**
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * March 2018
+ */
+public class AcquisitionStateResetScheduler extends SchedulerBase implements
+                                                               SchedulerInterface,
+                                                               LoggingFeature
+{
+  LightSheetMicroscope mLightSheetMicroscope;
+
+  /**
+   * INstanciates a virtual device with a given name
+   *
+   */
+  public AcquisitionStateResetScheduler()
+  {
+    super("Adaptation: Reset all control planes");
+  }
+
+  @Override public boolean initialize()
+  {
+    if (mMicroscope instanceof LightSheetMicroscope){
+      mLightSheetMicroscope = (LightSheetMicroscope) mMicroscope;
+    }
+    return true;
+  }
+
+  @Override public boolean enqueue(long pTimePoint)
+  {
+    InterpolatedAcquisitionState lAcquisitionState = (InterpolatedAcquisitionState)mLightSheetMicroscope.getAcquisitionStateManager().getCurrentState();
+
+    for (int lLightSheetIndex = 0; lLightSheetIndex < mLightSheetMicroscope.getNumberOfLightSheets(); lLightSheetIndex++) {
+      for (int cpi = 0; cpi < lAcquisitionState.getNumberOfControlPlanes(); cpi++) {
+        InterpolationTables it = lAcquisitionState.getInterpolationTables();
+        it.set(LightSheetDOF.IZ, cpi, lLightSheetIndex, 0);
+        it.set(LightSheetDOF.IY, cpi, lLightSheetIndex, 0);
+        it.set(LightSheetDOF.IX, cpi, lLightSheetIndex, 0);
+        it.set(LightSheetDOF.IA, cpi, lLightSheetIndex, 0);
+        it.set(LightSheetDOF.IW, cpi, lLightSheetIndex, 0.45); // Todo: this value is XWing specific
+        it.set(LightSheetDOF.IH, cpi, lLightSheetIndex, 500); // Todo: this value is XWing speciific
+      }
+    }
+
+    return true;
+  }
+}
