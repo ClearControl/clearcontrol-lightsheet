7737f9ab442fbb6b21a0179aeaadaefef6c691a2
haesleinhuepf <rhaase@mpi-cbg.de>
2018-05-03 17:59:47 +0200

    workaround: if the flat mirror configuration is not found in the warehouse, get it from disc.

--- src/clearcontrol/microscope/lightsheet/spatialphasemodulation/optimizer/fitness/MirrorModeImageQualityDeterminer.java
+++ src/clearcontrol/microscope/lightsheet/spatialphasemodulation/optimizer/fitness/MirrorModeImageQualityDeterminer.java
@@ -1,16 +1,20 @@
 package clearcontrol.microscope.lightsheet.spatialphasemodulation.optimizer.fitness;
 
+import clearcontrol.core.configuration.MachineConfiguration;
 import clearcontrol.core.log.LoggingFeature;
 import clearcontrol.microscope.lightsheet.LightSheetMicroscope;
 import clearcontrol.microscope.lightsheet.extendeddepthoffocus.iqm.DiscreteConsinusTransformEntropyPerSliceEstimator;
 import clearcontrol.microscope.lightsheet.imaging.SingleViewPlaneImager;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.MirrorModeContainer;
+import clearcontrol.microscope.lightsheet.spatialphasemodulation.io.DenseMatrix64FReader;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.SpatialPhaseModulatorDeviceInterface;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.zernike.TransformMatrices;
 import clearcontrol.microscope.lightsheet.state.schedulers.AcquisitionStateBackupRestoreScheduler;
 import clearcontrol.microscope.lightsheet.warehouse.containers.DataContainerInterface;
 import clearcontrol.stack.StackInterface;
 import org.ejml.data.DenseMatrix64F;
+
+import java.io.File;
 
 /**
  * MirrorModeImageQualityDeterminer
@@ -47,7 +51,18 @@ public class MirrorModeImageQualityDeterminer implements LoggingFeature {
             DenseMatrix64F lFlatMirrorMatrix = ((MirrorModeContainer) lContainer).getMirrorMode();
             lMatrixToTest = TransformMatrices.sum(lMatrixToTest, lFlatMirrorMatrix);
         } else {
-            warning("No flat mirror matix found!");
+            warning("No flat mirror matix found! Reading from disc");
+            File lMirrorModeDirectory =
+                    MachineConfiguration.get()
+                            .getFolder("MirrorModes");
+
+            DenseMatrix64F lFlatMirrorMatrix = mSpatialPhaseModulatorDeviceInterface.getMatrixReference().get().copy();
+            new DenseMatrix64FReader(new File(lMirrorModeDirectory, mSpatialPhaseModulatorDeviceInterface.getName() + "_flat.json"), lFlatMirrorMatrix).read();
+            lMatrixToTest = TransformMatrices.sum(lMatrixToTest, lFlatMirrorMatrix);
+
+            MirrorModeContainer lNewContainer = new MirrorModeContainer(mLightSheetMicroscope);
+            lNewContainer.setMirrorMode(lFlatMirrorMatrix);
+            mLightSheetMicroscope.getDataWarehouse().put(mSpatialPhaseModulatorDeviceInterface.getName() + "_flat", lNewContainer);
         }
 
         mSpatialPhaseModulatorDeviceInterface.getMatrixReference().set(lMatrixToTest);
