a572000dcda7fb3eebd2aea6b18d90c6ab513660
haesleinhuepf <rhaase@mpi-cbg.de>
2018-05-05 10:02:57 +0200

    code cleanup

--- src/clearcontrol/microscope/lightsheet/state/spatial/FOVBoundingBox.java
+++ src/clearcontrol/microscope/lightsheet/state/spatial/FOVBoundingBox.java
@@ -10,23 +10,16 @@ import clearcontrol.microscope.lightsheet.state.spatial.PositionListContainer;
  *
  * Todo: The FOV bonuding box should be available via DataWarehouse as a kind of persistent container which cannot
  * be recycled.
- * 
+ *
  * Author: @haesleinhuepf
  * 05 2018
  */
 public class FOVBoundingBox extends SpaceTravelScheduler {
-
-
-    BasicStageInterface mStageX = null;
-    BasicStageInterface mStageY = null;
-    BasicStageInterface mStageZ = null;
 
     public FOVBoundingBox(LightSheetMicroscope pLightSheetMicroscope) {
         super("FOV");
         setMicroscope(pLightSheetMicroscope);
         initialize();
     }
-
-
 
 }
