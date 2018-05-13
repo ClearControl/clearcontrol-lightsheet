c7bfbc3336cdb33a80009af123e3d9187fce0763
haesleinhuepf <rhaase@mpi-cbg.de>
2018-05-13 20:29:15 +0200

    improved Drosophila developmental stage classification bump version bintray upload

--- src/clearcontrol/microscope/lightsheet/smart/sampleselection/DrosophilaSelectSampleJustBeforeInvaginationScheduler.java
+++ src/clearcontrol/microscope/lightsheet/smart/sampleselection/DrosophilaSelectSampleJustBeforeInvaginationScheduler.java
@@ -7,15 +7,23 @@ import clearcontrol.microscope.lightsheet.component.scheduler.SchedulerBase;
 import clearcontrol.microscope.lightsheet.postprocessing.containers.DCTS2DContainer;
 import clearcontrol.microscope.lightsheet.state.spatial.Position;
 import clearcontrol.microscope.lightsheet.warehouse.DataWarehouse;
+import org.apache.commons.math.stat.descriptive.moment.Mean;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * SelectBestQualitySampleScheduler
- * <p>
- * <p>
- * <p>
+ * DrosophilaSelectSampleJustBeforeInvaginationScheduler
+ *
+ * This scheduler analyses recent DCTS2D measurements retrieved from the DataWarehouse. It checks if the following
+ * conditions are met:
+ * * A sample has quality above average (compared to all samples)
+ * * The quality at time points t-1, t-2, t-3 has a five fold smaller gradient than at time points t-4, t-5 and t-6
+ *   (on average)
+ *
+ * If these conditions are met, all spatial positions are removed from the SpaceTravelScheduler but the one which
+ * fulfilled the conditions. Then, imaging the single sample can continue.
+ *
  * Author: @haesleinhuepf
  * 05 2018
  */
@@ -44,7 +52,7 @@ public class DrosophilaSelectSampleJustBeforeInvaginationScheduler extends Sched
         LightSheetMicroscope lLightSheetMicroscope = (LightSheetMicroscope)mMicroscope;
         DataWarehouse lDataWarehouse = lLightSheetMicroscope.getDataWarehouse();
 
-        ArrayList<DCTS2DContainer> lQualityInSpaceList = lDataWarehouse.getContainers(DCTS2DContainer.class);
+        ArrayList<DCTS2DContainer> lQualityInSpaceList = lDataWarehouse.getContainers(DCTS2DContainer.class, true);
 
         if (lQualityInSpaceList.size() == 0) {
             lLightSheetMicroscope.getTimelapse().log("No measurements found. Measure DCTS2D before asking me where the best sample is.");
@@ -60,8 +68,13 @@ public class DrosophilaSelectSampleJustBeforeInvaginationScheduler extends Sched
             return true;
         }
 
+        if (lQualityInSpaceList.size() % lPositionList.size() != 0) {
+            info("Waiting for having same number of measurements for all samples.");
+            return true;
+        }
+
         int [] qualityMeasurementSizes = new int[lPositionList.size()];
-        double[][] lQualityGroupedBySample = new double[lPositionList.size()][lQualityInSpaceList.size() / lPositionList.size() + 1];
+        double[][] lQualityGroupedBySample = new double[lPositionList.size()][lQualityInSpaceList.size() / lPositionList.size()];
 
         //DCTS2DContainer lMaxmimumQualityContainer = lQualityInSpaceList.get(0);
         for (DCTS2DContainer lContainer : lQualityInSpaceList) {
@@ -70,7 +83,6 @@ public class DrosophilaSelectSampleJustBeforeInvaginationScheduler extends Sched
             //}
 
             int positionIndex = getClosestPositionIndex(lPositionList, new Position(lContainer.getX(), lContainer.getY(), lContainer.getZ()));
-
             lQualityGroupedBySample[positionIndex][qualityMeasurementSizes[positionIndex]] = lContainer.getMeasurement();
             qualityMeasurementSizes[positionIndex]++;
         }
@@ -82,19 +94,26 @@ public class DrosophilaSelectSampleJustBeforeInvaginationScheduler extends Sched
             lLightSheetMicroscope.getTimelapse().log("Q[" + i  +"]: " + Arrays.toString(qualityMeasurementSizes));
         }
 
-        if (maxTimePoints < 9) {
+        if (maxTimePoints < 7) {
             lLightSheetMicroscope.getTimelapse().log("not enough data yet");
             info("not enough data yet");
             return false;
         }
 
         Position lBestPosition = null;
-        for (int i = 1; i < qualityMeasurementSizes.length; i++) {
-            if (sampleIsJustBeforeInvagination(lQualityGroupedBySample[i], maxTimePoints)) {
-                //lBestPosition = lPositionList.get(i);
-                lLightSheetMicroscope.getTimelapse().log("Sample at position " + lPositionList.get(i) + " is apparently undergoing invagiation soon!");
+        for (int i = 0; i < qualityMeasurementSizes.length; i++) {
+            if (sampleQualityAboveMean(lQualityGroupedBySample, i, maxTimePoints - 1)) {
+                if (sampleIsJustBeforeInvagination(lQualityGroupedBySample[i], maxTimePoints)) {
+                    //lBestPosition = lPositionList.get(i);
+                    info("Sample at position " + lPositionList.get(i) + " is apparently undergoing invagiation soon!");
+                    lLightSheetMicroscope.getTimelapse().log("Sample at position " + lPositionList.get(i) + " is apparently undergoing invagiation soon!");
+                } else {
+                    info("Sample at position " + lPositionList.get(i) + " is NOT undergoing invagiation soon!");
+                    lLightSheetMicroscope.getTimelapse().log("Sample at position " + lPositionList.get(i) + " is NOT undergoing invagiation soon!");
+                }
             } else {
-                lLightSheetMicroscope.getTimelapse().log("Sample at position " + lPositionList.get(i) + " is NOT undergoing invagiation soon!");
+                info("Sample at position " + lPositionList.get(i) + " has too low quality!");
+                lLightSheetMicroscope.getTimelapse().log("Sample at position " + lPositionList.get(i) + " has too low quality!");
             }
         }
 
@@ -108,11 +127,23 @@ public class DrosophilaSelectSampleJustBeforeInvaginationScheduler extends Sched
         return true;
     }
 
+    private boolean sampleQualityAboveMean(double[][] pQualityGroupedBySample, int pSampleIndex, int pTimepoint) {
+        double[] lQualityMeasurements = new double[pQualityGroupedBySample.length];
+        for (int i = 0; i < lQualityMeasurements.length; i++) {
+            lQualityMeasurements[i] = pQualityGroupedBySample[i][pTimepoint];
+        }
+
+        double lMeanQuality = new Mean().evaluate(lQualityMeasurements);
+        double lQuality = pQualityGroupedBySample[pSampleIndex][pTimepoint];
+
+        return lQuality > lMeanQuality;
+    }
+
     private double[] derivative(double[] input) {
         double[] output = new double[input.length - 1];
 
         for (int i = 1; i < input.length; i++) {
-            output[i - 1] = input[i] - input[i - 1];
+            output[i - 1] = input[i - 1] - input[i];
         }
 
         return output;
@@ -120,20 +151,28 @@ public class DrosophilaSelectSampleJustBeforeInvaginationScheduler extends Sched
 
     private boolean sampleIsJustBeforeInvagination(double[] pQualityMeasurements, int maxTimePoints) {
 
+
+
+
+
         double[] qualityDerivative = derivative(pQualityMeasurements);
 
-        double averageLastThree = Math.abs(1.0 / 3.0 * (
-                qualityDerivative[maxTimePoints - 2] +
-                        qualityDerivative[maxTimePoints - 3] +
-                        qualityDerivative[maxTimePoints - 4]));
-        double averageThreeBefore = Math.abs(1.0 / 3.0 * (
-                qualityDerivative[maxTimePoints - 5] +
-                        qualityDerivative[maxTimePoints - 6] +
-                        qualityDerivative[maxTimePoints - 7]));
+        double averageLastThree = 1.0 / 3.0 * (
+                Math.abs(qualityDerivative[maxTimePoints - 2]) +
+                        Math.abs(qualityDerivative[maxTimePoints - 3]) +
+                                Math.abs(qualityDerivative[maxTimePoints - 4]));
+        double averageThreeBefore = 1.0 / 3.0 * (
+                Math.abs(qualityDerivative[maxTimePoints - 5]) +
+                        Math.abs(qualityDerivative[maxTimePoints - 6]) +
+                                Math.abs(qualityDerivative[maxTimePoints - 7]));
 
         // In case of a soon to start invagination we assume quality to be mostly stable. Furthermore, in the past,
         // quality is supposed to have changed a lot. Thus quality deltas should be very different (by a factor):
-        return averageThreeBefore / averageLastThree > 5;
+        double factor = averageThreeBefore / averageLastThree;
+        info("quality: " + Arrays.toString(pQualityMeasurements));
+        info("derivative: " + Arrays.toString(qualityDerivative));
+        info("Factor: " + factor);
+        return factor > 5;
     }
 
     private int getClosestPositionIndex(ArrayList<Position> lPositionList, Position lPosition) {
