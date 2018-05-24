9b6b1d52c58d99cfdf353a359b241b7b676b41ef
haesleinhuepf <rhaase@mpi-cbg.de>
2018-05-24 19:22:59 +0200

    implemented DMP40 mirror device

---
+++ src/clearcontrol/microscope/lightsheet/spatialphasemodulation/slms/devices/dmp40/DMP40Device.java
@@ -0,0 +1,61 @@
+package clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.devices.dmp40;
+
+import clearcontrol.core.log.LoggingFeature;
+import clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.ZernikeModeFactorBasedSpatialPhaseModulatorBase;
+import dmp40j.DMP40JDevice;
+
+/**
+ * DMP40Device
+ * <p>
+ * <p>
+ * <p>
+ * Author: @haesleinhuepf
+ * 05 2018
+ */
+public class DMP40Device extends ZernikeModeFactorBasedSpatialPhaseModulatorBase
+        implements LoggingFeature {
+
+    DMP40JDevice mDMP40JDevice;
+    public DMP40Device(String pSerialNumber) {
+        super("DMP40 " + pSerialNumber, 1, 1, 1);
+
+        mDMP40JDevice = new DMP40JDevice(pSerialNumber);
+    }
+
+    @Override
+    public void zero() {
+        mDMP40JDevice.sendFlatMirrorShapeVector();
+    }
+
+    @Override
+    public long getRelaxationTimeInMilliseconds() {
+        return mDMP40JDevice.getRelaxSteps(); // Todo: steps == seconds?
+    }
+
+    @Override
+    public boolean setZernikeFactors(double[] pZernikeFactors) {
+        mDMP40JDevice.setZernikeFactors(pZernikeFactors);
+        super.setZernikeFactorsInternal(pZernikeFactors);
+        return false;
+    }
+
+    @Override
+    public boolean start() {
+        return mDMP40JDevice.open();
+    }
+
+    @Override
+    public boolean stop() {
+        if (mDMP40JDevice.isOpen()) {
+            try {
+                mDMP40JDevice.close();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
