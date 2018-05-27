614a42268710a103f687a247c6e90c2fa571a9b6
haesleinhuepf <rhaase@mpi-cbg.de>
2018-05-27 13:18:58 +0200

    code cleanup, coments

--- src/clearcontrol/instructions/InstructionBase.java
+++ src/clearcontrol/instructions/InstructionBase.java
@@ -4,6 +4,12 @@ import clearcontrol.core.device.VirtualDevice;
 import clearcontrol.microscope.MicroscopeInterface;
 
 /**
+ * The Instruction base is used to have a common class for all instructions.
+ *
+ * Todo: Determine if deriving from VirtualDevice makes sense or not. Maybe,
+ * this link csn/should be removed after Instructions are no longer devices
+ * of the microscope
+ *
  * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
  * January 2018
  */
@@ -11,12 +17,11 @@ public abstract class InstructionBase extends VirtualDevice implements
         InstructionInterface
 {
   /**
-   * INstanciates a virtual device with a given name
    *
-   * @param pDeviceName device name
+   * @param pInstructionName instruction name
    */
-  public InstructionBase(String pDeviceName)
+  public InstructionBase(String pInstructionName)
   {
-    super(pDeviceName);
+    super(pInstructionName);
   }
 }
