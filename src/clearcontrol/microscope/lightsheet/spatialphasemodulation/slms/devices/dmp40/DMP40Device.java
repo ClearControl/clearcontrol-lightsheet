f5191a0bb351bf975f5ac9e3a8a5f96cda836f0f
haesleinhuepf <rhaase@mpi-cbg.de>
2018-05-29 15:38:47 +0200

    bugfix: send the Zernike factors to the mirror ordered as it expects it (something similar like jNoll(i) - 4);

--- src/clearcontrol/microscope/lightsheet/spatialphasemodulation/slms/devices/dmp40/DMP40Device.java
+++ src/clearcontrol/microscope/lightsheet/spatialphasemodulation/slms/devices/dmp40/DMP40Device.java
@@ -2,7 +2,10 @@ package clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.devices.d
 
 import clearcontrol.core.log.LoggingFeature;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.ZernikeModeFactorBasedSpatialPhaseModulatorBase;
+import clearcontrol.microscope.lightsheet.spatialphasemodulation.zernike.ZernikePolynomials;
 import dmp40j.DMP40JDevice;
+
+import static dmp40j.DMP40J.*;
 
 /**
  * DMP40Device
@@ -34,9 +37,68 @@ public class DMP40Device extends ZernikeModeFactorBasedSpatialPhaseModulatorBase
 
     @Override
     public boolean setZernikeFactors(double[] pZernikeFactors) {
-        mDMP40JDevice.setZernikeFactors(pZernikeFactors);
+
+        mDMP40JDevice.setZernikeFactors(convertZernikeArray(pZernikeFactors));
         super.setZernikeFactorsInternal(pZernikeFactors);
         return false;
+    }
+
+
+    /**
+     * We assume Zernike factors being entered in Noll-Order
+     * @param inputZernikeFactors
+     * @return
+     */
+    private double[] convertZernikeArray(double[] inputZernikeFactors) {
+        double[] output = new double[mDMP40JDevice.getNumberOfZernikeFactors()];
+
+        for (int i = 0; i < inputZernikeFactors.length; i++) {
+            int jNollZ = ZernikePolynomials.jNoll(i);
+            switch (jNollZ) {
+                case 4:
+                    output[TLDFMX_Z4_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 5:
+                    output[TLDFMX_Z5_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 6:
+                    output[TLDFMX_Z6_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 7:
+                    output[TLDFMX_Z7_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 8:
+                    output[TLDFMX_Z8_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 9:
+                    output[TLDFMX_Z9_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 10:
+                    output[TLDFMX_Z10_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 11:
+                    output[TLDFMX_Z11_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 12:
+                    output[TLDFMX_Z12_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 13:
+                    output[TLDFMX_Z13_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 14:
+                    output[TLDFMX_Z14_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                case 15:
+                    output[TLDFMX_Z15_AMPL_POS] = inputZernikeFactors[i];
+                    break;
+                default:
+                    if (Math.abs(inputZernikeFactors[i]) > 0.0001) {
+                        warning("Ignoring Z" + jNollZ + " = " + inputZernikeFactors[i]);
+                    }
+                    break;
+            }
+        }
+        return output;
     }
 
     @Override
