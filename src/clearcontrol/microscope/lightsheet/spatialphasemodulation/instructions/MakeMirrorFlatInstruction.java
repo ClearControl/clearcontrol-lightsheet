19ff763db70849a68b3c361e0cb12bbd6fd536ef
haesleinhuepf <rhaase@mpi-cbg.de>
2018-07-30 08:56:21 +0200

    cleaned code, added comments

--- src/clearcontrol/microscope/lightsheet/spatialphasemodulation/instructions/MakeMirrorFlatInstruction.java
+++ src/clearcontrol/microscope/lightsheet/spatialphasemodulation/instructions/MakeMirrorFlatInstruction.java
@@ -7,13 +7,17 @@ import clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.ZernikeMod
 
 import java.util.Random;
 
+/**
+ * The MakeMirrorFlatInstruction sends an array of zeros to the deformable mirror device in order
+ * to give it a flat shape.
+ *
+ * Author: @haesleinhuepf
+ * July 2018
+ */
 public class MakeMirrorFlatInstruction extends InstructionBase implements
         LoggingFeature {
 
     private ZernikeModeFactorBasedSpatialPhaseModulatorBase mZernikeModeFactorBasedSpatialPhaseModulatorBase;
-    private BoundedVariable<Double> mMaximumZernikeFactor = new BoundedVariable<Double>("Maximum Zernike factor", 5.0, -Double.MAX_VALUE, Double.MAX_VALUE, 0.0000001);
-    private BoundedVariable<Double> mMinimumZernikeFactor = new BoundedVariable<Double>("Minimum Zernike factor", -5.0, -Double.MAX_VALUE, Double.MAX_VALUE, 0.0000001);
-    private Random mRandom = new Random();
 
     public MakeMirrorFlatInstruction(ZernikeModeFactorBasedSpatialPhaseModulatorBase pZernikeModeFactorBasedSpatialPhaseModulatorBase) {
         super("Adaptive optics: Send flat mode to " + pZernikeModeFactorBasedSpatialPhaseModulatorBase.getName());
