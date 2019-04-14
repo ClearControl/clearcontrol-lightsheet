9ff8fff441fb957f690f1b17d01eca4a01da65fa
haesleinhuepf <rhaase@mpi-cbg.de>
2019-04-14 22:18:26 +0200

    bugfix: filter instruction didn't save all properties

--- src/main/java/clearcontrol/microscope/lightsheet/warehouse/instructions/FilterStacksInStackInterfaceContainerInstruction.java
+++ src/main/java/clearcontrol/microscope/lightsheet/warehouse/instructions/FilterStacksInStackInterfaceContainerInstruction.java
@@ -98,6 +98,7 @@ public class FilterStacksInStackInterfaceContainerInstruction extends
   public Variable[] getProperties()
   {
     return new Variable[]
-    { getFilter() };
+    { getFilter(),
+    getMatchExactly()};
   }
 }
